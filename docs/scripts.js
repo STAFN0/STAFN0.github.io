5b307db9ec782855946d8bdc14d589781a376f79
STAFN0 <pubgonlypubg11@gmail.com>
2024-06-28 00:52:49 +0300

    Update scripts.js

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,1 +1,20 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
 
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Dropdown animation
+const dropdownBtn = document.querySelector('.dropdown-btn');
+const dropdownContent = document.querySelector('.dropdown-content');
+const dropdownArrow = document.querySelector('.dropdown-btn img');
+
+dropdownBtn.addEventListener('click', function() {
+    dropdownContent.classList.toggle('show');
+    dropdownArrow.classList.toggle('rotate');
+});
